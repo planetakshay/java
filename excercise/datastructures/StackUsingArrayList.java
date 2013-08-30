da4a50d5d0dda9a2425be1bff33bb40b378c90c7
Akshay Deodhar <adeodhar1@yahoo.com>
2013-08-30 10:19:19 -0700

    StackUsingArrayList - was deleted somehow, adding it back

---
+++ excercise/datastructures/StackUsingArrayList.java
@@ -0,0 +1,16 @@
+package datastructures;
+
+import java.util.ArrayList;
+
+public class StackUsingArrayList <E> extends ArrayList<E>{
+	
+	public void push(E e){
+		add(e);
+	}
+	
+	public E pop(){
+		E e = get(size()-1);
+		remove(size()-1);
+		return e;
+	}
+}
