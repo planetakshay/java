d31d16ca6434828f259202f697b209fdb0d71a9c
Akshay <adeodhar1@yahoo.com>
2013-09-02 15:46:30 -0700

    pascal row

---
+++ excercise/Series/PascalRow.java
@@ -0,0 +1,33 @@
+package Series;
+
+public class PascalRow{
+	
+	public static void main(String args[]){
+		//Write a pascal triangle for n number of rows
+		int n=15;
+		int[] row = new int[0];
+		//Find out the next row
+		for(int i=1;i<n;i++){
+			//find next row
+			row =  pascalRow(row);
+			for(int j=0;j<row.length;j++){
+				System.out.print(row[j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	public static int[] pascalRow(int[] previous){
+		
+		int[] row = new int[previous.length+1];
+		row[0]=1;
+		row[row.length-1]=1;
+		for(int i=1;i<row.length-1;i++){
+			row[i]=previous[i-1]+previous[i];
+		}
+		return row;
+	}
+	
+	
+	
+	
+}
