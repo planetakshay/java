63bdadccdd4e0da53f556efd58875e26526b8c3a
Akshay <adeodhar1@yahoo.com>
2013-09-02 15:57:21 -0700

    reverse the number

---
+++ excercise/MathProblems/ReverseTheNumber.java
@@ -0,0 +1,19 @@
+package MathProblems;
+
+public class ReverseTheNumber {
+
+	public static void main(String args[]){
+		int n=193324521;
+		int digit=0;
+		int reverse=0;
+		while(n!=0){
+			digit = n%10;
+			reverse = reverse *10 + digit;
+			n =n/10;
+			//System.out.print(n+" ");
+		}
+
+		System.out.println(reverse);
+	}
+	
+}
