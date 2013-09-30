84265693337d07c161acada412ad93d8441bbae2
adeodhar1 <adeodhar1@yahoo.com>
2013-09-29 17:39:59 -0700

    substitute %20 instead of space using string builder

---
+++ excercise/StringOperations/substituteTwentyAtSpace.java
@@ -0,0 +1,32 @@
+package StringOperations;
+/**
+ * This program substitutes  %20 every time it encounters space.
+ * 
+ * @author Akshay
+ *
+ */
+public class substituteTwentyAtSpace {
+
+	public static void main(String args[]){
+		String str = "This is a string with spaces.";
+		String newStr = substituteWithStringBuilder(str);
+		System.out.println(newStr);
+		
+	}
+	
+	public static String substituteWithStringBuilder(String str){
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<str.length()-1;i++){
+			char c = str.charAt(i);
+			if(String.valueOf(c).equalsIgnoreCase(" ")){
+				sb.append("%20");
+			}
+			else{
+				sb.append(c);
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+}
