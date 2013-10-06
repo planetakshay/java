31edba98000d22c65cac7d5718e772ced9b8e50e
Akshay Deodhar <adeodhar1@yahoo.com>
2013-10-06 11:30:28 -0700

    more tests

--- excercise/datastructures/tests/LinkListOpsTest.java
+++ excercise/datastructures/tests/LinkListOpsTest.java
@@ -24,12 +24,40 @@ public class LinkListOpsTest {
 		l.addLinkToHead(1);
 		l.addLinkToHead(2);
 		l.addLinkToHead(3);
-		
-		
-		
 		assertTrue("testAddToHead Failed exLink="+exLink.toString() + "and actual linklist ="+l.getLinkListData().toString(), exLink.toString().equalsIgnoreCase(l.getLinkListData().toString()));
-		System.out.println(l.getLinkListData().toString());
+		System.out.println("testAddToHead passed" +l.getLinkListData().toString());
 		System.out.println("----");
-		
 	}
+	@Test
+	public void testAddToTail(){
+		ArrayList<Integer> exLink = new ArrayList<Integer>();
+		exLink.add(1);
+		exLink.add(2);
+		exLink.add(3);
+		
+		LinkListOps l = new LinkListOps();
+		l.addLinkToTail(1);
+		l.addLinkToTail(2);
+		l.addLinkToTail(3);
+		assertTrue("testAddToTail Failed exLink="+exLink.toString() + "and actual linklist ="+l.getLinkListData().toString(), exLink.toString().equalsIgnoreCase(l.getLinkListData().toString()));
+		System.out.println("testAddToTail passed" +l.getLinkListData().toString());
+		System.out.println("----");
+	}
+	
+	@Test
+	public void testAddToHeadTailHead(){
+		ArrayList<Integer> exLink = new ArrayList<Integer>();
+		exLink.add(3);
+		exLink.add(1);
+		exLink.add(2);
+		
+		LinkListOps l = new LinkListOps();
+		l.addLinkToHead(1);
+		l.addLinkToTail(2);
+		l.addLinkToHead(3);
+		assertTrue("testAddToHeadTailHead Failed exLink="+exLink.toString() + "and actual linklist ="+l.getLinkListData().toString(), exLink.toString().equalsIgnoreCase(l.getLinkListData().toString()));
+		System.out.println("testAddToHeadTailHead passed" +l.getLinkListData().toString());
+		System.out.println("----");
+	}
+	
 }
