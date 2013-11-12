e8c05fdd6d393690b23b0bfe5e4813150f71098e
adeodhar1 <adeodhar1@yahoo.com>
2013-11-12 12:12:48 -0800

    Adding height left and height right
    height left and height right

--- excercise/datastructures/BinarySearchTree.java
+++ excercise/datastructures/BinarySearchTree.java
@@ -16,44 +16,64 @@ public class BinarySearchTree {
 		node = bst.insert(node, 10);
 		node = bst.insert(node, 16);
 		node = bst.insert(node, 26);
+		node = bst.insert(node, 0);
+		node = bst.insert(node, 2);
+		node = bst.insert(node, 3);
+		node = bst.insert(node, 5);
+		node = bst.insert(node, 9);
+		node = bst.insert(node, 100);
 		
 		bst.printTreeLayers(rootNode);
+		System.out.println();
+		System.out.println("heightLeft="+bst.heightLeft(rootNode));
+		System.out.println("heightLeft="+bst.heightRight(rootNode));
 	}
 
-public TreeStructure insert(TreeStructure node, int value){
-	if(node==null){
-		node = new TreeStructure();
-		node.data=value;
-		
-	}else{
-		if(node.data>value){
-			node.left =insert(node.left,value);
+	public TreeStructure insert(TreeStructure node, int value){
+		if(node==null){
+			node = new TreeStructure();
+			node.data=value;
 			
-		}else if(node.data<=value){
-			node.right=insert(node.right,value);
+		}else{
+			if(node.data>value){
+				node.left =insert(node.left,value);
+				
+			}else if(node.data<=value){
+				node.right=insert(node.right,value);
+			}
 		}
-	}
-	return node;
- }
+		return node;
+	 }
 
-public void printTreeLayers(TreeStructure root){
-	
-	Queue<TreeStructure> tree = new LinkedList<TreeStructure>();
-	tree.add(root);
-	
-	while(!tree.isEmpty()){
-		TreeStructure node = tree.poll();
-		System.out.print(node.data +", ");
-		if(node.left !=null){
-			tree.add(node.left);
+	public void printTreeLayers(TreeStructure root){
+		
+		Queue<TreeStructure> tree = new LinkedList<TreeStructure>();
+		tree.add(root);
+		
+		while(!tree.isEmpty()){
+			TreeStructure node = tree.poll();
+			System.out.print(node.data +", ");
+			if(node.left !=null){
+				tree.add(node.left);
+			}
+			if(node.right !=null){
+				tree.add(node.right);
+			}
 		}
-		if(node.right !=null){
-			tree.add(node.right);
+		
+	 }
+	public int heightLeft(TreeStructure node){
+		if(node==null){
+			return 0;
 		}
+		return 1+heightLeft(node.left);
 	}
-	
-	
- }
+	public int heightRight(TreeStructure node){
+		if(node==null){
+			return 0;
+		}
+		return 1+heightRight(node.right);
+	}
 
 }
 
