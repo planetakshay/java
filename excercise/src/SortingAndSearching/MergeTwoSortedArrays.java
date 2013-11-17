bdaf0dff4c285980ed764f09ea2d19d1b67192cc
Akshay <Akshay@Deodhar.att.net>
2013-11-16 19:06:00 -0800

    Merge Two Sorted Arrays

---
+++ excercise/src/SortingAndSearching/MergeTwoSortedArrays.java
@@ -0,0 +1,42 @@
+package SortingAndSearching;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+	//Given two sorted array, merge them in ascending order
+	public int[] merge(int[] a, int[] b){
+		int m= a.length;
+		int n= b.length;
+		int[] mergedArray = new int[m+n];
+		int i=m-1; //index of last element in a
+		int j=n-1;// index of last element in b
+		int k=m+n-1; //index of last element of merged array
+		
+		while(i>=0 && j>=0){
+			if(a[i] >= b[j]){
+				mergedArray[k--]=a[i--];
+			}else{
+				mergedArray[k--]=b[j--];
+			}
+			
+		}
+		if(i>=0){
+			while(i>=0){
+				mergedArray[k--] = a[i--];
+			}
+		}
+		if(j>=0){
+			while(j>=0){
+				mergedArray[k--]= b[j--];
+			}
+		}
+		return mergedArray;
+	}
+
+	public static void main(String args[]){
+		int[] a = new int[]{4,7,9};
+		int[] b = new int[]{1,10,11};
+		int[] merged = new MergeTwoSortedArrays().merge(a, b);
+		System.out.println(Arrays.toString(merged));
+	}
+}
