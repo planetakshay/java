f94132ae4a445d798688e40a280db1a9fa3aa9f4
Akshay <Akshay@Deodhar.att.net>
2013-12-07 20:44:57 -0800

    some modifications

--- excercise/src/Math/PrimeNumbers.java
+++ excercise/src/Math/PrimeNumbers.java
@@ -1,6 +1,8 @@
 package Math;
 /**
- * Print first N prime number
+ * Print first N prime number.
+ * 
+ * Definition of Prime number: Any number greater than 1 that is divisible by number itself and 1 and no other number is a prime numer
  * Amazon phone interview - 2013
  * 
  * @author Akshay
@@ -9,7 +11,7 @@ package Math;
 public class PrimeNumbers {
 
 	public static boolean isPrime(int x){
-		if(x==0 || x==1){ // Any number is always divisible by 1. 0 and 1 are not part of prime numbers
+		if(x==0 || x==1 ){ // Any number is always divisible by 1. 0 and 1 are not part of prime numbers
 			return false;
 		}
 		for(int i=2;i<=Math.sqrt(x);i++){ //Important thing to remember is loop starts at 2
@@ -27,7 +29,7 @@ public class PrimeNumbers {
 		while(i<=n){
 			
 			if(isPrime(number)){
-				System.out.println(i+"="+number+",");
+				System.out.println(i+"="+number+" ");
 				i++;
 			}
 			number++;
