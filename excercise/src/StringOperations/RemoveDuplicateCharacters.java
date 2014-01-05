ed85855a167de075f0d1ddb1ace0ac67e4fdc1c6
Akshay <Akshay@Deodhar.att.net>
2014-01-05 13:55:39 -0800

    Added a new method using string builder

--- excercise/src/StringOperations/RemoveDuplicateCharacters.java
+++ excercise/src/StringOperations/RemoveDuplicateCharacters.java
@@ -8,6 +8,28 @@ package StringOperations;
  */
 public class RemoveDuplicateCharacters {
 	
+public static String removeDuplicateCharacters(String s){
+		
+		StringBuilder sb = new StringBuilder();
+		int tail=0;
+		for(int i=0;i<s.length();i++){
+			char c = s.charAt(i);
+			boolean found = false;
+			for(int j=0;j<tail;j++){
+				char ch = s.charAt(j);
+				if(ch==c){
+					found=true;
+					break;
+				}
+			}
+			if(!found){
+				sb.append(c);
+			}
+			tail++;
+		}
+		return sb.toString();
+	}
+
 	public static String removeDup(String str){
 		if(str==null ){
 			return "";
