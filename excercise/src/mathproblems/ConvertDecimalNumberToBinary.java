dcf5a42b05d09e9112bedefeab8f50671ce2099a
Akshay <Akshay@Deodhar.att.net>
2014-01-14 06:14:24 -0800

    package name change

---
+++ excercise/src/mathproblems/ConvertDecimalNumberToBinary.java
@@ -0,0 +1,23 @@
+package mathproblems;
+
+public class ConvertDecimalNumberToBinary {
+	public static void printBinary(int i){
+		StringBuffer sb = new StringBuffer();
+		int rem=0;
+		while(i>0){
+			rem = i%2; // reminder will be either 1 or 0
+			sb.append(rem);
+			i=i/2; // number will decrease and finally we will have 1/2 and i will be 0.
+		}
+		sb.reverse();
+		System.out.println(sb.toString());
+	}
+	public static void main(String args[]){
+		int array[]  = new int[]{2,6,4,5,11,16,3}; 
+		
+		for(int i: array){
+			printBinary(i);
+		}
+	}
+
+}
