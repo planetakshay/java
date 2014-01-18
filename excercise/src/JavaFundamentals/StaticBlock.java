9cf7ab987743dbba117722297e534461aaf6a998
Akshay <Akshay@Deodhar.att.net>
2014-01-17 21:49:48 -0800

    static block

---
+++ excercise/src/JavaFundamentals/StaticBlock.java
@@ -0,0 +1,31 @@
+package JavaFundamentals;
+
+class TestStatic{
+	// static block is executed before even the constructor
+	static{
+		System.out.println("This is static block1 of TestStatic");
+	}
+	TestStatic(){
+		System.out.println("constructor:TestStatic");
+	}
+	static{
+		System.out.println("This is static block2 of TestStatic");
+	}
+}
+public class StaticBlock {
+	int day;
+	static{
+		System.out.println("This is static block 1 of StaticBlock");
+	}
+	
+	StaticBlock(int day){
+		System.out.println("constructor:BlockStatic with day="+day);
+		this.day=day;
+	}
+
+	public static void main(String args[]){
+		
+		StaticBlock sb = new StaticBlock(25);
+		TestStatic ts = new TestStatic();
+	}
+}
