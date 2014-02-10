1b9edf0b5294e0471afaec08a1538559294eee25
adeodhar1 <adeodhar1@yahoo.com>
2014-02-09 18:21:28 -0800

    more comments and precize code

--- excercise/src/StringOperations/ReplaceAll.java
+++ excercise/src/StringOperations/ReplaceAll.java
@@ -11,22 +11,19 @@ import java.util.ArrayList;
  */
 public class ReplaceAll {
 	
-	
-
-	//given a string s1,s2, finds if s1 is a substring of s2.
+	//given a string s1,s2, find all indexes where s1 occurs in s2.
 		public static ArrayList<Integer> getSubStringIndexes(char[] s1, char[] s2){
+			
+			if((s1!=null && s2!=null) && ( s2.length<s1.length || s1.length==0 )) return new ArrayList<Integer>();
+			
 			ArrayList<Integer> indexes = new ArrayList<Integer>();
 			for(int i=0;i<s2.length;i++){
 				
-				if(s1[0]==s2[i] && s2.length-i>=s1.length){
+				if(s1[0]==s2[i] && s2.length-i>=s1.length){ // if the first character matches and part of s2 thats remaining to be processed is greater than the s1.length
 					int originalIndex = i;
-					
-					for(int j=0;j<s1.length;j++,originalIndex++){
-						if(s2[originalIndex]!=s1[j]){
-							break;
-						}else if(j==s1.length-1){
-							indexes.add(i);
-						}
+					for(int j=0;j<s1.length;j++){
+						if(s2[originalIndex++]!=s1[j]) break;
+						if(j==s1.length-1) indexes.add(i); // Add the start index at which the match was found
 					}
 				}
 			}
@@ -34,12 +31,15 @@ public class ReplaceAll {
 		}
 	public static String replaceAll(char[] s1,char[] tobeReplaced,char[] replaceWith  ){
 		ArrayList<Integer> indexes = getSubStringIndexes(tobeReplaced,s1);
+		//if tobeReplaced not found in s1, return original string back.
+		if(indexes.size() == 0) return String.valueOf(s1);
+		
 		char [] result = new char[Math.abs(s1.length +indexes.size()*replaceWith.length - (indexes.size()*tobeReplaced.length))];
 		
 		for(int i=0,ni=0;ni<result.length;ni++,i++){
 			if(indexes.contains(i)){
 				
-				for(int j=0;j<replaceWith.length;j++){
+				for(int j=0;j<replaceWith.length;j++){ // add replace with to result string.
 					result[ni++]=replaceWith[j];
 				}
 				i=i-1+tobeReplaced.length; // (i-1) to bring for loop back to one character before the match was found
@@ -75,5 +75,22 @@ public class ReplaceAll {
 		System.out.println("replacing "+tobeReplaced +" with "+replaceWith);
 		System.out.println(replaceAll(s1.toCharArray(),tobeReplaced.toCharArray(),replaceWith.toCharArray()));
 		System.out.println("--");
+		
+		s1 = "Today is day.";
+		tobeReplaced = "day";
+		replaceWith = "";
+		System.out.println("In-> "+s1);
+		System.out.println("replacing "+tobeReplaced +" with "+replaceWith);
+		System.out.println(replaceAll(s1.toCharArray(),tobeReplaced.toCharArray(),replaceWith.toCharArray()));
+		System.out.println("--");
+		
+		s1 = "Today is day.";
+		tobeReplaced = "";
+		replaceWith = "";
+		System.out.println("In-> "+s1);
+		System.out.println("replacing "+tobeReplaced +" with "+replaceWith);
+		System.out.println(replaceAll(s1.toCharArray(),tobeReplaced.toCharArray(),replaceWith.toCharArray()));
+		System.out.println("--");
+		
 	}
 }
