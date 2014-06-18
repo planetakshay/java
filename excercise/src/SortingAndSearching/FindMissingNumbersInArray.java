90c1219325b8e788fa07c680dc7ea50ce3dff388
adeodhar1 <adeodhar1@yahoo.com>
2014-06-17 22:12:19 -0700

    bug fixes

--- excercise/src/SortingAndSearching/FindMissingNumbersInArray.java
+++ excercise/src/SortingAndSearching/FindMissingNumbersInArray.java
@@ -12,14 +12,18 @@ public class FindMissingNumbersInArray {
 	public static void printMissingNumbersUsingSort(int[] a){
 		Arrays.sort(a);// You could use quick sort for that.
 		int j=0;
-		for(int i=1;i<=a[a.length-1];i++){
-			
-			if(i!=a[j]){
-				System.out.print( i+" ");
+		int lastFoundIndex=0;
+		for(int i=0;i<=a[a.length-1];i++){
+			boolean found =false;
+			for(j=lastFoundIndex;j<a.length;j++){
+				if(a[j]==i){
+					found=true;
+					lastFoundIndex = j;
+				}
 			}
-			if(i==a[j]){
-				j++;
-			}
+			if(!found){
+				System.out.print(i+" ");
+			}	
 		}
 		System.out.println();
 	}
@@ -36,19 +40,21 @@ public class FindMissingNumbersInArray {
 				freqMap.put(a[i], true);
 			}
 		}
-		for(int i=1;i<=max;i++){
+		for(int i=0;i<=max;i++){
 			if(!freqMap.containsKey(i)){
 				System.out.print(i+" ");
 			}
 		}
-		
+		System.out.println();	
 	}
 	public static void main(String args[]){
 		
-		int[] a = new int[]{2,5,1,10,7,15};
+		int[] a = new int[]{2,5,1,3,3,10,7,15};
 		printMissingNumbersUsingSort(a);
 		printMissingNumbersUsingMap(a);
-		
+		a = new int[]{2,15,7,5,1,10,7,15};
+		printMissingNumbersUsingSort(a);
+		printMissingNumbersUsingMap(a);
 	}
 	
 	
