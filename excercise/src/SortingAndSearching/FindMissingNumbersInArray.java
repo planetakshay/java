f38ac4f7184a9d0764b56f2cccc5b7ae925074ad
adeodhar1 <adeodhar1@yahoo.com>
2014-06-19 05:57:21 -0700

    simple method to find missing elements

--- excercise/src/SortingAndSearching/FindMissingNumbersInArray.java
+++ excercise/src/SortingAndSearching/FindMissingNumbersInArray.java
@@ -9,6 +9,21 @@ import java.util.*;
 
 public class FindMissingNumbersInArray {
 
+	public static void printMissingNumberUsingSortAndWhile(int[] a ){
+		Arrays.sort(a);// You could use quick sort for that.
+		int j=0;
+		for(int i=0;i<=a[a.length-1];i++){
+			//if array element matches i, then if there are more elements with same value, increment j index
+			if(a[j]==i ){
+				while(j<a.length && a[j]==i  ) j++; 
+				continue;
+			}
+			else{ //if i does not match with a[j] then just print it
+				System.out.print(i+" ");
+			}
+		}
+		System.out.println("printMissingNumberUsingSortAndWhile is complete");
+	}
 	public static void printMissingNumbersUsingSort(int[] a){
 		Arrays.sort(a);// You could use quick sort for that.
 		int j=0;
@@ -16,16 +31,18 @@ public class FindMissingNumbersInArray {
 		for(int i=0;i<=a[a.length-1];i++){
 			boolean found =false;
 			for(j=lastFoundIndex;j<a.length;j++){
-				if(a[j]==i){
+				if(a[j]==i){  //if we find the number in array, turn on the flag, store the index and break
 					found=true;
 					lastFoundIndex = j;
+					break;
 				}
+				if(a[j]>i) break; // if the array element is already greater than i, break;
 			}
 			if(!found){
 				System.out.print(i+" ");
 			}	
 		}
-		System.out.println();
+		System.out.println("printMissingNumbersUsingSort complete");
 	}
 	
 	public static void printMissingNumbersUsingMap(int[]a){
@@ -45,14 +62,17 @@ public class FindMissingNumbersInArray {
 				System.out.print(i+" ");
 			}
 		}
-		System.out.println();	
+		System.out.println("printMissingNumbersUsingMap is complete");	
 	}
 	public static void main(String args[]){
 		
 		int[] a = new int[]{2,5,1,3,3,10,7,15};
+		printMissingNumberUsingSortAndWhile(a);
 		printMissingNumbersUsingSort(a);
 		printMissingNumbersUsingMap(a);
+		
 		a = new int[]{2,15,7,5,1,10,7,15};
+		printMissingNumberUsingSortAndWhile(a);
 		printMissingNumbersUsingSort(a);
 		printMissingNumbersUsingMap(a);
 	}
