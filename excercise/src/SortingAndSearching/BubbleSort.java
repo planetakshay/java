2b6460a608a750d5bf0ac1e8bb0410d934c4291a
adeodhar1 <adeodhar1@yahoo.com>
2014-07-24 11:05:30 -0700

    clearer code

--- excercise/src/SortingAndSearching/BubbleSort.java
+++ excercise/src/SortingAndSearching/BubbleSort.java
@@ -11,13 +11,15 @@ public class BubbleSort {
 	
 	public static void sort(int x[]){
 		
-		 int n = x.length;
-		    for (int pass=1; pass < n; pass++) {  // count how many times
+		 
+		    for (int pass=1; pass < x.length; pass++) {  // count how many times
 		        // This next loop becomes shorter and shorter
-		        for (int i=0; i < n-pass; i++) {
+		        for (int i=0; i < x.length-pass; i++) {
 		            if (x[i] > x[i+1]) {
 		                // exchange elements
-		                int temp = x[i];  x[i] = x[i+1];  x[i+1] = temp;
+		                int temp = x[i];  
+		                x[i] = x[i+1];  
+		                x[i+1] = temp;
 		            }
 		        }
 		    }
