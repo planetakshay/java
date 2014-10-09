7f2516968ade558f081825ec76f7d3aadbda5e72
adeodhar1 <adeodhar1@yahoo.com>
2014-10-09 07:20:25 -0700

    more methods on bst

--- excercise/src/datastructures/BinarySearchTree.java
+++ excercise/src/datastructures/BinarySearchTree.java
@@ -3,90 +3,26 @@ package datastructures;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
+
+class TreeStructure{
+	int data;
+	TreeStructure left;
+	TreeStructure right;
+}
 public class BinarySearchTree {
-	private class TreeStructure{
-		int data;
-		TreeStructure left;
-		TreeStructure right;
+	
+	public int treeDiameter(TreeStructure node){
 		
-	}
-	public static void main(String args[]){
-		BinarySearchTree bst = new BinarySearchTree();
-		TreeStructure node=null;
-		node = bst.insert(node, 10);
-		TreeStructure rootNode = node;
-		node = bst.insert(node,5);
-		node = bst.insert(node, 8);
-		node = bst.insert(node, 12);
-		node = bst.insert(node, 10);
-		node = bst.insert(node, 16);
-		node = bst.insert(node, 26);
-		node = bst.insert(node, 0);
-		node = bst.insert(node, 2);
-		node = bst.insert(node, 3);
-		node = bst.insert(node, 5);
-		node = bst.insert(node, 9);
-		node = bst.insert(node, 100);
-		
-		bst.printTreeLayers(rootNode);
-		System.out.println();
-		System.out.println("heightLeft="+bst.heightLeft(rootNode));
-		System.out.println("height right="+bst.heightRight(rootNode));
-		System.out.println("max depth="+bst.maxDepth(rootNode));
-		System.out.println("min depth="+bst.minDepth(rootNode));
-		
-	}
-
-	public TreeStructure insert(TreeStructure node, int value){
-		if(node==null){
-			node = new TreeStructure();
-			node.data=value;
-			
-		}else{
-			if(node.data>value){
-				node.left =insert(node.left,value);
-				
-			}else if(node.data<=value){
-				node.right=insert(node.right,value);
-			}
-		}
-		return node;
-	 }
-
-	public void printTreeLayers(TreeStructure root){
-		
-		Queue<TreeStructure> tree = new LinkedList<TreeStructure>();
-		tree.add(root);
-		//Testing
-		while(!tree.isEmpty()){
-			TreeStructure node = tree.poll();
-			System.out.print(node.data +", ");
-			if(node.left !=null){
-				tree.add(node.left);
-			}
-			if(node.right !=null){
-				tree.add(node.right);
-			}
-		}
-		
-	 }
-	public int heightLeft(TreeStructure node){
 		if(node==null){
 			return 0;
 		}
-		return 1+heightLeft(node.left);
-	}
-	public int heightRight(TreeStructure node){
-		if(node==null){
-			return 0;
-		}
-		return 1+heightRight(node.right);
-	}
-	public int maxDepth(TreeStructure  node){
-		if(node==null){
-			return 0;
-		}
-		return 1+Math.max(maxDepth(node.left), maxDepth(node.right));
+		int lHeight = heightLeft(node.left);
+		int rHeight = heightRight(node.right);
+		int lDiameter = treeDiameter(node.left);
+		int rDiameter = treeDiameter(node.right);
+		return Math.max(lHeight+rHeight+1, Math.max(lDiameter, rDiameter));
+		
 	}
 	public int minDepth(TreeStructure node){
 		if(node==null){
@@ -94,9 +30,82 @@ public class BinarySearchTree {
 		}
 		return 1+Math.min(minDepth(node.left), minDepth(node.right));
 	}
+	public int maxDepth(TreeStructure node){
+		if(node==null){
+			return 0;
+		}
+		return 1+Math.max(maxDepth(node.left),maxDepth(node.right));
+	}
+	public int heightRight(TreeStructure node){
+		if(node==null){
+			return 0;
+		}
+		return 1+heightRight(node.right);
+	}
+	public int heightLeft(TreeStructure node){
+		if(node==null){
+			return 0;
+		}
+		return 1+heightLeft(node.left);
+	}
+	public void inOrderTraversal(TreeStructure node ){
 	
-	
+		if(node == null) return;
+		inOrderTraversal(node.left);
+		System.out.println(node.data+" ");
+		inOrderTraversal(node.right);
+	}
+	public void printAllLayers(TreeStructure node){
+		System.out.println("Printing All Layers");
+		
+		Queue<TreeStructure> queue = new LinkedList<TreeStructure>();
+		queue.add(node);
+		while(!queue.isEmpty()){
+			node =queue.poll();
+			System.out.println(node.data+" ");
+			if(node.left!=null){
+				queue.add(node.left);
+			}
+			if(node.right!=null){
+				queue.add(node.right);
+			}
+		}
+		
+	}
+	public TreeStructure insert(TreeStructure node,int value){
+		
+		if(node==null){
+			node = new TreeStructure();
+			node.data  = value;
+		}else if(value<node.data){
+			node.left = insert(node.left,value);
+		}else if(value>node.data){
+			node.right = insert(node.right,value);
+		}
+		return node;
+	}
+	public static void main(String args[]){
+		TreeStructure rootNode = null;
+		TreeStructure node =null;
+		BinarySearchTree bst = new BinarySearchTree();
+		node = bst.insert(rootNode,20);
+		TreeStructure root = node;
+		node = bst.insert(node,10);
+		node = bst.insert(node, 5);
+		node = bst.insert(node, 30);
+		node = bst.insert(node, 25);
+		node = bst.insert(node, 35);
+		node = bst.insert(node, 45);
+		node = bst.insert(node, 55);
+		System.out.println();
+		bst.printAllLayers(root);
+		System.out.println("In order Traversal");
+		bst.inOrderTraversal(node);
+		System.out.println("Height Left");
+		System.out.println(bst.heightLeft(root));
+		System.out.println("Height Right");
+		System.out.println(bst.heightRight(root));
+		System.out.println("Tree Diameter");
+		System.out.println(bst.treeDiameter(root));
+	}
 }
-
-
-	
