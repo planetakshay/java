33cdb09baf0900bae3e85194824a5b3b41f5f7f6
Akshay Deodhar <akshay_deodhar@intuit.com>
2015-06-07 14:41:32 -0700

    efficient way of finding two max numbers

--- excercise/src/mathproblems/FindMaximumNumber.java
+++ excercise/src/mathproblems/FindMaximumNumber.java
@@ -12,6 +12,7 @@ import java.util.List;
  */
 public class FindMaximumNumber {
 	
+	//returns the maximum amount.
 	public static int getMax(int[] ary){
 		if(ary.length <= 0) return 0;
 	
@@ -25,6 +26,7 @@ public class FindMaximumNumber {
 		return max;
 	}
 	
+	//Method-1 but uses two for loops.
 	public static List<Integer> getTwoMax(int[] ary){
 	
 		List<Integer> twoMax = new ArrayList<Integer>();
@@ -49,21 +51,21 @@ public class FindMaximumNumber {
 		twoMax.add(max2);
 		return twoMax;
 	}
-	//does not work. working on it
+	
+	// Most efficient way of doing this. uses one for loop.
 	public static List<Integer> getTwoMaxOneForLoop(int[] ary){
 		
 		List<Integer> twoMax = new ArrayList<Integer>();
 		
 		int max1=Math.max(ary[0], ary[1]);
 		int max2=Math.min(ary[0], ary[1]);
-		int diff = Math.abs(max1-max2);
-		
+		int diff = Math.abs(max1-max2);	
 		for(int i=1;i<ary.length;i++){
-			
+			if(ary[i] > max2 && ary[i] <max1){
+				max2 = ary[i];
+				continue;
+			}
 			if(ary[i] > max1){
-				if(Math.abs(ary[i]-max2) > diff){
-					max2 = max1;
-				}
 				max1 = ary[i];
 			}
 		}
@@ -79,6 +81,7 @@ public class FindMaximumNumber {
 		System.out.println(getMax(array));
 		System.out.println(getTwoMax(array));
 		System.out.println(getTwoMaxOneForLoop(array));
+		
 	}
 	
 }
