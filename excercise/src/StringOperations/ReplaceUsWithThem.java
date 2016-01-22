b11b9b8abd0d1f3238030b0e003bff65d8454f6e
Akshay Deodhar G <akshay_deodhar@intuit.com>
2016-01-22 05:47:47 -0800

    removed comment

--- excercise/src/StringOperations/ReplaceUsWithThem.java
+++ excercise/src/StringOperations/ReplaceUsWithThem.java
@@ -7,12 +7,12 @@ public class ReplaceUsWithThem {
 		//String  regex="us|US";
 	
 	//	str = str.replaceAll(regex, "them");
-	//	str = str.replaceAll("(^| )us($| )", " them ").trim(); //Got this from Prachi
-		str = str.replaceAll("$us", "*them*");
+	//	str = str.replaceAll("(^| )us($| )", " them ").trim();
+		str = str.replaceAll("us|US", "*them*");
 		return str;
 	}
 	public static void main(String args[]){
-		String str = "us We are here and they will give it to us us. They said to us, how are you. US and us";
+		String str = "us We are here and they will give it to us us. They said to us, how are you. US and US";
 		System.out.println(ReplaceUsWithThem.replace(str));	
 	}
 }
