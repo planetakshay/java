3baa9eb9ea3c6b7adf56f1976209c91657b1d63e
Akshay Deodhar G <akshay_deodhar@intuit.com>
2016-11-05 14:47:23 -0700

    Use of join

--- excercise/src/ThreadProblems/CounterNumbers.java
+++ excercise/src/ThreadProblems/CounterNumbers.java
@@ -25,7 +25,16 @@ public class CounterNumbers {
 	public static void main (String args[]){
 		Thread t1 = new Thread(new CounterThread("First Counter",100));
 		Thread t2 = new Thread(new CounterThread("Second Counter",150));
+		System.out.println("Starting both threads");
 		t1.start();
 		t2.start();
+		System.out.println("Waiting for both threads to finish");
+		try{
+			t1.join();
+			t2.join();
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		System.out.println("Both thread finished executing");
 	}
 }
