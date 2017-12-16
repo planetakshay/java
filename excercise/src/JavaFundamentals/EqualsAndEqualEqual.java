4b22519a537cc7a3c0ed19dc833993f0b4c6fec3
Akshay Deodhar <akshaydeodhar@google.com>
2017-12-16 14:47:33 -0800

    more comments for better understanding

--- excercise/src/JavaFundamentals/EqualsAndEqualEqual.java
+++ excercise/src/JavaFundamentals/EqualsAndEqualEqual.java
@@ -7,16 +7,22 @@ public class EqualsAndEqualEqual {
 		   String s1 ="abc";
 		   String s2 = s1;
 		   String s3 = "abc";
+
+		   //For a primitive type - reference comparision via == or object comparison bia .equals method both result in same answer - that is true
 		 	 System.out.println("== comparison : " + (s1 == s3));
 		   System.out.println("== comparison : " + (s1 == s2));
 
 		   System.out.println("Using equals method : " + s1.equals(s2));
+		   System.out.println("Using equals method : " + s1.equals(s3));
 
 
+		   //For a String non primitive type - actual object - reference comparision results in answer false whereas object content comparision via .equals method results answer true
 		   String s44 = new String("abc");
 		   String s55 = new String("abc");
-		   System.out.println("== comparison : " + (s44 == s55));
-		   System.out.println(" == comparison : " + String.valueOf(s44==s55));
-		   System.out.println("Using equals method : " + s44.equals(s55));
+		   System.out.println("== comparison : " + (s44 == s55));  //false - because both objects have different references
+		   System.out.println("Using equals method : " + s44.equals(s55)); // true -  becasue both objects have same content 'abc'
+
+
+
 	 }
 }
